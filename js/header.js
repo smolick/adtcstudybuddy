e181dce7e88e0edb6609aa8387d04cdc0214c546
smolick <smolick@gmail.com>
2025-05-02 19:51:31 -0700

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,8 +1,7 @@
 // header.js
 export async function loadHeader() {
-  const resp = await fetch('header.html');
-  if (!resp.ok) throw new Error('Could not load header');
-  const html = await resp.text();
-  // insert it at the top of <body>
-  document.body.insertAdjacentHTML('afterbegin', html);
+	const resp = await fetch('header.html');
+	if (!resp.ok) throw new Error('Could not load header');
+		const html = await resp.text();
+	document.body.insertAdjacentHTML('afterbegin', html);
 }
