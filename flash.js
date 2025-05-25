33699ef979fa16498c52e79a6d330050b4ad13f1
smolick <smolick@gmail.com>
2025-05-25 06:23:12 -0700

    Update flash.js
    commas added

--- flash.js
+++ flash.js
@@ -17,9 +17,9 @@ const prettyNames = {
 	theatmosphere:				"The Atmosphere",
 	envoytest2024:				"Airline Test 2024",
 	tafalternates:				"Do I Need an Alternate?",
-	ilslocapproach:				"ILS or LOC Approach (Image)"
-	atc:					"ATC"
-	airspace:				"Airspaces"
+	ilslocapproach:				"ILS or LOC Approach (Image)",
+	atc:					"ATC",
+	airspace:				"Airspaces",
 	flightlevels:				"Flight Levels"
 	
   };
