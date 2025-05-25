771402eaa9c3973ed548552588c1f9d991a8bdd2
smolick <smolick@gmail.com>
2025-05-25 06:42:55 -0700

    Update atc.js
    Spelling and styling

--- flashcards/atc.js
+++ flashcards/atc.js
@@ -1,20 +1,20 @@
 // flashcards/atc.js
 export const atc = [
-      { front : `What is a TMU? Where are they located?`, back: `Traffic Management Unit. Located in ARTCCs (and some TRACONs)` },
+      { front : `What is a TMU? Where are they located?`, back: `<b>T</b>raffic <b>M</b>anagement <b>U</b>nit. Located in ARTCCs (and some TRACONs)` },
       { front : `What do TMUs issue?`, back: `They issue ground stops and ground delay programs` },
       { front : `What are the 2 types of radar ATC uses?`, back: `Primary and secondary` },
       { front : `What does a Clearance Delivery controller provide to a flightcrew?`, back: `<ul><li>Clearance limit (usually the destination)</li>
 <li>Route of flight</li>
 <li>Transponder code</li></ul>` },
-      { front : `What is TRACON?`, back: `Terminal Radar Approach Control` },
+      { front : `What is TRACON?`, back: `<b>T</b>erminal <b>R</b>adar <b>A</b>pproach <b>Con</b>trol` },
       { front : `What does a TRACON provide?`, back: `It provides approach and departure control services for an aircraft entering and exiting terminal areas` },
-      { front : `What is TMI?`, back: `Traffic Management Initiatives` },
+      { front : `What is TMI?`, back: `<b>T</b>raffic <b>M</b>anagement <b>I</b>nitiatives` },
       { front : `Ground stops and ground delays are issued by who?`, back: `Traffic Management Units (TMU) at the ARTCC (or some TRACONs)` },
       { front : `What does primary radar provide to ATC?`, back: `Aircraft position` },
       { front : `How early can you file a flight plan with ATC?`, back: `30 minutes` },
-      { front : `What is ASDE?`, back: `Airport Surface Detection Equipment` },
+      { front : `What is ASDE?`, back: `<b>A</b>irport <b>S</b>urface <b>D</b>etection <b>E</b>quipment` },
       { front : `What does ASDE provide ATC?`, back: `The ability to track an aircraft on the ground via the aircrafts transponder` },
-      { front : `What does secondary radar provide to ATC?`, back: `Thansponder information, ex; position, altitude, and call sign` },
+      { front : `What does secondary radar provide to ATC?`, back: `Transponder information, ex; position, altitude, and call sign` },
       { front : `Is an average heading of 360° considered eastbound or westbound?`, back: `Eastbound` },
       { front : `What are the 3 non-discreet transponder codes?`, back: `<ul><li>7500 - Hijacking</li>
 <li>7600 - Loss of radio communications</li>
@@ -23,11 +23,11 @@ export const atc = [
       { front : `What are the requirements when crossing and ADIZ over water?`, back: `To cross within 5 minutes and 20 NM of where you planned/stated your intention to cross` },
       { front : `What are the universal emergency radio frequencies?`, back: `VHF: 121.5<br>
 UHF: 243` },
-      { front : `What is an ADIZ? What does it provide?`, back: `Air Defense Identificaition Zone. It's an airspace over land or water, within which the identification, location, and control of civil aircraft is performed in the interest of national security` },
+      { front : `What is an ADIZ? What does it provide?`, back: `<b>A</b>ir <b>D</b>efense <b>I</b>dentificaition Zone. It's an airspace over land or water, within which the identification, location, and control of civil aircraft is performed in the interest of national security` },
       { front : `How long after ETD does ATC clearance expire?`, back: `2 Hours` },
       { front : `How long before ETD does ATC provide clearance for departure?`, back: `10 Minutes` },
       { front : `How does ATC identify a specific aircraft via its transponder?`, back: `By issuing a four-digit "squawk" code unique to that aircraft, to which the pilot(s) set their transponder(s)` },
       { front : `Are westbound flights planned to cruise at odd- or even-numbered flight levels?`, back: `Even-numbered` },
-      { front : `What is a LAHSO?`, back: `Land and Hold Short Operations` },
+      { front : `What is a LAHSO?`, back: `<b>L</b>and <b>A</b>nd <b>H</b>old <b>S</b>hort <b>O</b>perations` },
       { front : `How does LAHSO work?`, back: `A point in which an aircraft landing must come to a complete stop (during specific hours), generally before the intersection of 2 runways` },
 ];
