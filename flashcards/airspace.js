31cc803b6d43f9577699cccdf31157ca5286e4e6
smolick <smolick@gmail.com>
2025-05-25 06:18:30 -0700

    Create airspace.js
    new flashcards

---
+++ flashcards/airspace.js
@@ -0,0 +1,36 @@
+// flashcards/airspace.js
+export const airspace = [
+      { front : `What flight levels is Class A?`, back: `FL180 to FL600` },
+      { front : `What class airspace is RVSM located in?`, back: `Class A` },
+      { front : `What is classified as a prohibited airspace?`, back: `An airspace where aircraft are not allowed (More severe than a restricted airspace)` },
+      { front : `What altitude does Class C cover?`, back: `4000' AGL` },
+      { front : `What altitude does Class E cover?`, back: `0' MSL to FL180, and anything above FL600` },
+      { front : `What is required for an aircraft to enter Class D?`, back: `Two-way Radio communication` },
+      { front : `What is an alert area?`, back: `A type of restricted airspace, similar to a Military Operations Area (MOA)` },
+      { front : `What are the requirements when crossing an ADIZ over land?`, back: `To cross within 5 minutes and 10 NM of where you planned/stated your intention to cross` },
+      { front : `What is RVSM airspace? What does it provide?`, back: `Reduced Vertical Separation Minima. It reduces the required vertical separation between aircraft from 2000' to 1000'` },
+      { front : `What flight levels is RVSM airspace?`, back: `FL290 to FL410.` },
+      { front : `Can you enter a MOA on a VFR flight plan?`, back: `Yes` },
+      { front : `What is a MOA? How does it work?`, back: `A Military Operations Area. It's an airspace designated to separate military activities from IFR traffic, and to inform VFR traffic where these activities are being conducted` },
+      { front : `What equipment is required to fly in RVSM airspace?`, back: `<ul><li>2RAC</li>
+<li>Transponder with altitude-encoding (mode-c)</li>
+<li>2 independent altimeters</li>
+<li>1 altitude reporting system</li>
+<li>1 altitude alerting sstem</li>
+<li>1 automatic altitude control system (ex; autopilot</li></ul>` },
+      { front : `What is Class E airspace?`, back: `Any controlled airspace within the United States that is not Class A, B, C, or D` },
+      { front : `What is required for an aircraft to enter Class C?`, back: `A transponder with mode-c and two-way radio communications` },
+      { front : `What is the height of Class D?`, back: `2500' AGL` },
+      { front : `What airspace class is RVSM located?`, back: `It's found in Class A airspace` },
+      { front : `Can a VFR flight operate in Class A airspace?`, back: `No. Class A airspace requires IFR` },
+      { front : `Where can Class C airspace be found?`, back: `Around relatively busy airports that are less busy than those foun in Class B airspace` },
+      { front : `What is the maximum speed permitted when traveling under a "shelf" in Class B airspace?`, back: `200kts` },
+      { front : `What is standard holding speed above 14000' MSL?`, back: `265kts` },
+      { front : `What equipment is required to enter Class B?`, back: `A transponder with mode-c and ATC clearance` },
+      { front : `What is Class G airspace?`, back: `Uncontrolled airspace from ground level to 700' AGL (for instrument approach procedures), or 1200' AGL (when adjacent B, C, or D airspace), or up to 14,000MSL next to mountainous terrain` },
+      { front : `How does ATC identify a specific aircraft via its transponder?`, back: `By issuing a four-digit "squawk" code unique to that aircraft, to which the pilot(s) set their transponder(s)` },
+      { front : `Where is Class B airspace found?`, back: `Around the country's largest and/or busiest airports` },
+      { front : `What is the minimum speed required when arriving into Class B airspace?`, back: `210kts` },
+      { front : `What is the maximum speed permitted in Class D airspace?`, back: `200kts` },
+      { front : `What is the maximum speed permitted in Class C airspace?`, back: `200kts` },
+];
