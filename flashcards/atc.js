ef290f86c00f52f911cf0d565598b5577c4c1ff1
smolick <smolick@gmail.com>
2025-05-25 08:14:07 -0700

    Update atc.js
    styling fixes

--- flashcards/atc.js
+++ flashcards/atc.js
@@ -3,9 +3,7 @@ export const atc = [
       { front : `What is a TMU? Where are they located?`, back: `<b>T</b>raffic <b>M</b>anagement <b>U</b>nit. Located in ARTCCs (and some TRACONs)` },
       { front : `What do TMUs issue?`, back: `They issue ground stops and ground delay programs` },
       { front : `What are the 2 types of radar ATC uses?`, back: `Primary and secondary` },
-      { front : `What does a Clearance Delivery controller provide to a flightcrew?`, back: `<p><ul><li>Clearance limit (usually the destination)</li>
-<li>Route of flight</li>
-<li>Transponder code</li></ul></p>` },
+      { front : `What does a Clearance Delivery controller provide to a flightcrew?`, back: "<ul><li>Clearance limit (usually the destination)</li><li>Route of flight</li><li>Transponder code</li></ul>" },
       { front : `What is TRACON?`, back: `<b>T</b>erminal <b>R</b>adar <b>A</b>pproach <b>Con</b>trol` },
       { front : `What does a TRACON provide?`, back: `It provides approach and departure control services for an aircraft entering and exiting terminal areas` },
       { front : `What is TMI?`, back: `<b>T</b>raffic <b>M</b>anagement <b>I</b>nitiatives` },
@@ -19,9 +17,8 @@ export const atc = [
       { front : `What are the 3 non-discreet transponder codes?`, back: "<ul><li>7500 - Hijacking</li><li>7600 - Loss of radio communications</li><li>7700 - All other emergencies</li></ul>" },
       { front : `What does the Tower controller provide to the flight crew?`, back: `Takeoff and landing clearance` },
       { front : `What are the requirements when crossing and ADIZ over water?`, back: `To cross within 5 minutes and 20 NM of where you planned/stated your intention to cross` },
-      { front : `What are the universal emergency radio frequencies?`, back: `<p>VHF: 121.5<br>
-UHF: 243</p>` },
-      { front : `What is an ADIZ? What does it provide?`, back: `<b>A</b>ir <b>D</b>efense <b>I</b>dentificaition <b>Z</b>one. It's an airspace over land or water, within which the identification, location, and control of civil aircraft is performed in the interest of national security` },
+      { front : `What are the universal emergency radio frequencies?`, back: "VHF: 121.5<br>UHF: 243" },
+      { front : `What is an ADIZ? What does it provide?`, back: "<b>A</b>ir <b>D</b>efense <b>I</b>dentificaition <b>Z</b>one.<br> It's an airspace over land or water, within which the identification, location, and control of civil aircraft is performed in the interest of national security" },
       { front : `How long after ETD does ATC clearance expire?`, back: `2 Hours` },
       { front : `How long before ETD does ATC provide clearance for departure?`, back: `10 Minutes` },
       { front : `How does ATC identify a specific aircraft via its transponder?`, back: `By issuing a four-digit "squawk" code unique to that aircraft, to which the pilot(s) set their transponder(s)` },
